0a8dc6e62718c516c6f5aa480e073edc62e711c5
Afaq Ahmed <afaq.ahmed.retailo.co>
2021-10-21 14:49:03 +0500

    Schema Designed

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,17 +1,4 @@
 'use strict';
-/*module.exports = function(app) {
-  let user = require('../controllers/userController');
-
-  // user Routes
-  app.route('/consumers')
-    .get(user.list_all_consumers)
-
-  app.route('/saleagents')
-    .get(user.list_all_saleagents)
-    
-  
-
-};*/
 
 const express = require('express');
 const router = express.Router();
