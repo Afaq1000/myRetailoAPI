2a54defb469e5bfa046f52e56eed3ba40195c52f
Afaq Ahmed <afaq.ahmed.retailo.co>
2021-12-03 18:19:47 +0500

    connenction string bug fixed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,11 @@
-mongoose = require('mongoose'),
-Task = require('../api/models'), 
+const mongoose = require('mongoose');
+const Task = require('../api/models');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/myRetailodb'); 
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/myRetailodb'); 
+async function any(){
+await mongoose.connect(
+    `mongodb+srv://afaq4542:Planec130@myretailocluster.si0n3.mongodb.net/myRetailoDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+};
+any();
